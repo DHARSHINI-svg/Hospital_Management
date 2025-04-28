7429cdce5832269a0a667f5b76f59e1e49abd533
Dharshini S <dharshinis616@gmail.com>
2025-04-28 15:39:14 +0530

    Update HospitalManagementSystem.java

--- HospitalManagementSystem.java
+++ HospitalManagementSystem.java
@@ -1,46 +1,36 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
 class Person {
     protected String name;
     protected int age;
-
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 }
-
 class Patient extends Person {
     private String id;
-    private String disease;
-    private String contact;
-
+    public String disease;
+    public String contact;
     public Patient(String id, String name, int age, String disease, String contact) {
         super(name, age);
         this.id = id;
         this.disease = disease;
         this.contact = contact;
     }
-
     public String getId() {
         return id;
     }
-
     public void displayInfo() {
         System.out.println("Patient[ID:" + id + ", Name: " + name + ", Age: " + age +", Disease: " + disease + ", Contact: " + contact+"]");
-    }
-    public String toCSV()
-    {
-        return id+","+age+","+disease+","+contact;
     }
 }
 class Doctor extends Person
     {
-        private String id;
-        private String specialization;
-        private String contact;
-        public Doctor(String id,String name;int age;String specialization;String contact)
+        public String id;
+        public String specialization;
+        public  String contact;
+        public Doctor(String id,String name,int age,String specialization,String contact)
         {
             super(name,age);
             this.id=id;
@@ -55,16 +45,11 @@ class Doctor extends Person
         {
             System.out.println("Doctor[ID:" + id +", Name: " + name + ", Age:"+ age+ ", Specialization: " + specialization + ", Contact: " + contact+"]");
         }
-public String toCSV()
-        {
-            return id+","+name+","+age+","+specialization+","+contact;
-        }}
-
+    }
 class HospitalManager {
     private ArrayList<Patient> patients = new ArrayList<>();
     private ArrayList<Doctor>doctors=new ArrayList<>();
     private Scanner sc = new Scanner(System.in);
-//..............PATIENT MANAGEMENT..........//
     public void addPatient() {
         System.out.print("Enter Patient ID: ");
         String id = sc.nextLine();
@@ -72,7 +57,6 @@ class HospitalManager {
             System.out.println("Patient ID already exists.");
             return;
         }
-
         System.out.print("Enter Name: ");
         String name = sc.nextLine();
         System.out.print("Enter Age: ");
@@ -84,7 +68,7 @@ class HospitalManager {
         patients.add(new Patient(id, name, age, disease, contact));
         System.out.println("Patient added successfully!");
     }
-    public void viewPatients() {
+    public void viewPatient() {
         if (patients.isEmpty()) {
             System.out.println("No patients found.");
         } else {
@@ -97,7 +81,7 @@ public void updatePatient()
     {
         System.out.print("Enter Patient ID to update:");
         String id=sc.nextLine();
-        Patient p=findPAtientById(id);
+        Patient p=findPatientById(id);
         if(p==null)
         {
             System.out.println("Patient not found!");
@@ -126,22 +110,6 @@ public void updatePatient()
         patients.remove(p);
         System.out.println("Patient deleted successfully!");
     }
-    public void searchPatient()
-        {
-        System.out.print("Enter Patient ID or NAme to search:");
-        String keyword=sc.nextLine().toLowerCase();
-            boolean found=false;
-            for(Patient p:patients)
-                {
-                    if(p.getId().equalsIgnoreCase(keyword)||p.name.toLowerCase()contains(keyword))
-                    {
-                        p.dispalyInfo();
-                        found=true;
-                    }}
-            if(!found)
-            {
-                System.out.println("Patient not found!");
-            }}
     private Patient findPatientById(String id) {
         for (Patient p : patients) {
             if (p.getId().equalsIgnoreCase(id)) {
@@ -150,21 +118,34 @@ public void updatePatient()
         }
         return null;
     }
-}
+    private int validateAge()
+    {
+        try
+            {
+int age=Integer.parseInt(sc.nextLine());
+                if(age<=0)
+                {
+                    throw new NumberFormatException();
+                }}
+catch(NumberFormatException e)
+            {
+                System.out.println("invalid age");
+            }
+			return 0;
+}}
+
 public class HospitalManagementSystem {
     public static void main(String[] args) {
         HospitalManager hm = new HospitalManager();
         Scanner sc = new Scanner(System.in);
         int choice=0;
-
         do {
             System.out.println("\n--- Hospital Management System ---");
             System.out.println("1. Add Patient");
-            System.out.println("2. View Patients");
-            System.out.println("3. Update Patients");
-            System.out.println("4. Delete Patients");
-            System.out.println("5. Search Patients");
-            System.out.println("6. Exit");
+            System.out.println("2. View Patient");
+            System.out.println("3. Update Patient");
+            System.out.println("4. Delete Patient");
+            System.out.println("5. Exit");
             System.out.print("Enter choice: ");
             try {
                 choice = Integer.parseInt(sc.nextLine());
@@ -172,13 +153,12 @@ public class HospitalManagementSystem {
                 System.out.println("Invalid input. Please enter a number.");
                 continue;
             }
-
             switch (choice) {
                 case 1:
                     hm.addPatient(); 
                     break;
                 case 2:
-                    hm.viewPatients(); 
+                    hm.viewPatient(); 
                     break;
                 case 3:
                     hm.updatePatient();
@@ -187,16 +167,12 @@ public class HospitalManagementSystem {
                     hm.deletePatient();
                     break;
                 case 5:
-                    hm.searchPatient();
-                    break;
-                case 6:
                     System.out.println("Exiting...");
                     break;
                 default: 
                     System.out.println("Invalid choice.");
             }
-        } while (choice !=6);
-
+        } while (choice !=5);
         sc.close();
     }
 }
